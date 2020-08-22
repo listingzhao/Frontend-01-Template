79989f4fdb903494f1c2ae7eb8e9180258e1ad01
Listing <listing.zhao@gmail.com>
2020-08-22 10:42:35 +0800

    工具链yomen generators 目录及初始化

---
+++ week19/toy-tool/generators/app/templates/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-react-jsx', { pragma: 'createElement' }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new (require('html-webpack-plugin'))
+  ],
+  optimization: {
+    minimize: false
+  },
+  devServer: {
+    open: true,
+    compress: false,
+    contentBase: './src'
+  }
+}
